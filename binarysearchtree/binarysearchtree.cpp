bfb45700e1190825fe141be19aca538c13b05f49
andiniseptia <andiniseptiandri124@gmail.com>
2023-07-02 09:21:55 +0700

    Menambahkan fungsi void insert

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -26,5 +26,13 @@ public:
 		ROOT = NULL;  // Initializing ROOT to null
 	}
 
+	void insert(string element)  // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL);  // Allocate memory the new node
+		newNode->info = element;  // Assign value to the data field of the new node
+		newNode->leftchild = NULL;  // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL;  // Make the right child of the new node point to NULL
+
 	
+	}
 };
